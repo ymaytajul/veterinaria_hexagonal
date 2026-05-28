fe0ee6e0b8955b617fda5b0027d088e3998e63a0
Yerson <julius3201.m@gmail.com>
2026-05-28 11:12:00 -0500

    bootstrap fix

--- bootstrap.php
+++ bootstrap.php
@@ -54,7 +54,7 @@ loadEnv(__DIR__ . '/.env');
 $pdo = new PDO(
     "mysql:host={$_ENV['DB_HOST']};port={$_ENV['DB_PORT']};dbname={$_ENV['DB_NAME']}",
     $_ENV['DB_USER'],
-    $_ENV['DB_PASS']
+    $_ENV['DB_PASSWORD']
 );
 
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
